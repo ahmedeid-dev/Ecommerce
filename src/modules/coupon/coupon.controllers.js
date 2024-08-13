32ed3f4ec68187f4785cc7e6e33772dd09b42f03
ahmedeid2260 <a.email2260@gmail.com>
2024-08-13 15:45:50 +0300

    coupon controller

--- src/modules/coupon/coupon.controllers.js
+++ src/modules/coupon/coupon.controllers.js
@@ -1,11 +1,13 @@
 import Coupon from './../../../database/models/coupons.model.js';
 import apiFeatures from '../../../utils/apiFeatures.js';
 import catchError from '../../../utils/catchError.js';
+import appError from '../../../utils/appError.js';
 
 // ! getCoupons controller
 const getCoupons = catchError(async (req, res, next) => {
     const features = apiFeatures(Coupon.find(), req.query)
     let coupons = await features.query;
+    !coupons && next(new appError("coupons not found", 404));
     res.status(200).json({
         status: "success", meta: {
             page: features.page,
@@ -17,24 +19,35 @@ const getCoupons = catchError(async (req, res, next) => {
 // ! getCoupon controller
 const getCoupon = catchError(async (req, res, next) => {
     const coupon = await Coupon.findById(req.params.id);
+    !coupon && next(new appError("coupon not found", 404));
     res.status(200).json({ status: "success", coupon });
 })
 
 // ! addCoupon controller
 const addCoupon = catchError(async (req, res, next) => {
-    const coupon = await Coupon.create(req.body);
+    const coupon = await Coupon.create({
+        code: req.body.code,
+        discount: req.body.discount,
+        expiry: req.body.expiry
+    });
     res.status(201).json({ status: "coupon added successfully", coupon });
 })
 
 // ! updateCoupon controller
 const updateCoupon = catchError(async (req, res, next) => {
-    const coupon = await Coupon.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    const coupon = await Coupon.findById(req.params.id)
+    !coupon && next(new appError("coupon not found", 404));
+    req.body.code && (coupon.code = req.body.code);
+    req.body.discount && (coupon.discount = req.body.discount);
+    req.body.expiry && (coupon.expiry = req.body.expiry);
+    await coupon.save();
     res.status(200).json({ status: "coupon updated successfully", coupon });
 })
 
 // ! deleteCoupon controller    
 const deleteCoupon = catchError(async (req, res, next) => {
     const coupon = await Coupon.findByIdAndDelete(req.params.id);
+    !coupon && next(new appError("coupon not found", 404));
     res.status(200).json({ status: "coupon deleted successfully", coupon });
 })
 
@@ -43,5 +56,5 @@ export {
     getCoupon,
     addCoupon,
     updateCoupon,
-    deleteCoupon
+    deleteCoupon,
 }

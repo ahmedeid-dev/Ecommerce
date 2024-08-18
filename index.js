3ab99d83eadda7b42d6c99cc910cc0927aaa2d03
ahmedeid2260 <a.email2260@gmail.com>
2024-08-18 22:06:20 +0300

    cors

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ import serverConnection from "./database/serverConnection.js"
 import bootstrap from './src/bootstrap.js';
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
+
 const app = express()
 const port = process.env.port || 3000
 dotenv.config({ path: "./config/.env" })
-
+app.use(cors())
 app.use(express.json())
 app.use(express.static("uploads"))
 bootstrap(app)
